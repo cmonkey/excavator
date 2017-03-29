fb4f62a0717af796c4ed4e007d8d3c8672e42164
CMonkey <42.codemonkey@gmail.com>
2017-03-30 00:06:47 +0800

    change get zkConnection

--- webboot/src/main/java/com/excavator/rpc/boot/SpringServerConfig.java
+++ webboot/src/main/java/com/excavator/rpc/boot/SpringServerConfig.java
@@ -3,6 +3,7 @@ package com.excavator.rpc.boot;
 import com.excavator.rpc.boot.service.SayService;
 import com.excavator.rpc.boot.service.impl.SayServiceImpl;
 import com.excavator.rpc.factory.ServerFactoryBean;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -13,8 +14,14 @@ import org.springframework.context.annotation.Bean;
 @SpringBootApplication
 public class SpringServerConfig {
 
+    @Value("${zkConnection}")
+    private String zkConnection;
+
+    @Value("${serviceName}")
+    private String serviceName;
+
     @Bean
-    public SayService hello(){
+    public SayService sayService(){
         return new SayServiceImpl();
     }
     @Bean
@@ -23,9 +30,9 @@ public class SpringServerConfig {
         final ServerFactoryBean serverFactoryBean = new ServerFactoryBean();
         serverFactoryBean.setPort(9090);
         serverFactoryBean.setServiceInterface(SayService.class);
-        serverFactoryBean.setServiceName("hello");
-        serverFactoryBean.setServiceImpl(hello());
-        serverFactoryBean.setZkConn("127.0.0.1:2181");
+        serverFactoryBean.setServiceName(serviceName);
+        serverFactoryBean.setServiceImpl(sayService());
+        serverFactoryBean.setZkConn(zkConnection);
 
         new Thread(() -> {
 
