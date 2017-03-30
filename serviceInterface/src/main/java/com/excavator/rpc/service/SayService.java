96a6cd2332d815484a46eab1b1462121deeeb9d2
CMonkey <42.codemonkey@gmail.com>
2017-03-30 11:35:32 +0800

    add SayService for serviceInterface

---
+++ serviceInterface/src/main/java/com/excavator/rpc/service/SayService.java
@@ -0,0 +1,12 @@
+package com.excavator.rpc.service;
+
+/**
+ * Created by cmonkey on 17-3-30.
+ */
+public interface SayService {
+    String say(String hello);
+
+    int sum(int a, int b);
+
+    int max(Integer a, Integer b);
+}
